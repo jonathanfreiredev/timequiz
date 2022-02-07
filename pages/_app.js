a99f38e30de62df9a7eac6524ee721965c937426
Jonathan Freire Espinoza <jonathan.freireespinoza@gmail.com>
2022-02-07 20:38:29 +0100

    Add cookiehub to the project as a cookie solution

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
+  const urlCookieHubDev = "https://dash.cookiehub.com/dev/";
+    const urlCookieHubProd = "https://cookiehub.net/c2/";
+    const dev = process.env.NODE_ENV === "development" ? urlCookieHubDev : urlCookieHubProd;
   return <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
@@ -10,6 +13,13 @@ function MyApp({ Component, pageProps }) {
       <link rel="preconnect" href="https://fonts.gstatic.com"></link>
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;900&display=swap" rel="stylesheet"></link>
       <link rel="icon" href="/favicon/favicon.ico" />
+      <script 
+        type="text/javascript" 
+        dangerouslySetInnerHTML={{ __html: `
+          let cpm={};(function(a,c,d){var b=a.getElementsByTagName("script")[0];a=a.createElement("script");a.async=!0;a.src="${dev}cd2129af.js";a.onload=function(){c.cookiehub.load(d)};b.parentNode.insertBefore(a,b)})(document,window,cpm);
+        `}}
+      >
+      </script>
     </Head>
     <Provider
     // Provider options are not required but can be useful in situations where
